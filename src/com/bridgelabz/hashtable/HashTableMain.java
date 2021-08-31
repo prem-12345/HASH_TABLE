f05379253ba69844ebade9a29ed4fc55bede7225
prem-12345 <premkpatil1997@gmail.com>
2021-08-31 12:46:40 +0530

    Added find frequency of words method

--- src/com/bridgelabz/hashtable/HashTableMain.java
+++ src/com/bridgelabz/hashtable/HashTableMain.java
@@ -3,9 +3,19 @@ package com.bridgelabz.hashtable;
 public class HashTableMain {
     public static void main(String[] args) {
 
-        // Welcome to Hashtable program
+         // Welcome to Hashtable program
+
+        HashTable hashTable = new HashTable(10);
+        hashTable.put(10, "To");
+        hashTable.put(20, "be");
+        hashTable.put(30, "or");
+        hashTable.put(40, "not");
+        hashTable.put(50, "to");
+        hashTable.put(60, "be");
+
+        System.out.println(hashTable.freqChecker("be"));
+
+
 
     }
 }
-
-
